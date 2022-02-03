e0563205b2abf9876333f14e361d9e3876e3f442
maotcha <azhsu2014@gmail.com>
2022-02-03 10:52:48 -0800

    fixing typo

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -18,7 +18,7 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks() throws IOException {
-        String fileContents = Files.readString(Path.of("/Users/aprilhsu/Documents/GitHub/markdown-parse/test3.md"));
+        String fileContents = Files.readString(Path.of(test3.md));
         ArrayList<String> testOutput = MarkdownParse.getLinks(fileContents.split("\n"));
         assertTrue("ArrayList should contain no elements", testOutput.isEmpty());
     }
