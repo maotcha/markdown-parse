1a2c69ee8c3d138b2b5acbcd98fe4fffcb8b1781
maotcha <azhsu2014@gmail.com>
2022-02-24 13:06:30 -0800

    created file and test for report 4 first code snippet

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -30,6 +30,16 @@ public class MarkdownParseTest {
         assertEquals("Check first element in array", "math18.com", testOutput.get(0));
         assertEquals("Array should only contain one element", 1, testOutput.size());
     }
+
+    @Test
+    public void testLabReportTest1() throws IOException {
+        String fileContents = Files.readString(Path.of("lab-report-test-1.md"));
+        ArrayList<String> testOutput = MarkdownParse.getLinks(fileContents.split("\n"));
+        assertEquals("Check first element in array", "`google.com", testOutput.get(0));
+        assertEquals("Check second element in array", "google.com", testOutput.get(1));
+        assertEquals("Check third element in array", "ucsd.edu", testOutput.get(2));
+        assertEquals("Array should contain three elements", 3, testOutput.size());
+    }
 }
 
 
